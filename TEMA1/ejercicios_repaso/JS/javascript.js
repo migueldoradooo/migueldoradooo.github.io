2fedf1f0b3d863090001600b2eb62a858bbd41c6
migueldoradooo <migueldoradomoreno57@gmail.com>
2025-12-09 17:21:05 +0100

    Delete TEMA1 directory

--- TEMA1/ejercicios_repaso/JS/javascript.js
+++
@@ -1,6 +0,0 @@
-function changeContent(elementClicked) {
-            
-            const newContent = elementClicked.getAttribute('data-content');
-            const mainContentElement = document.getElementById('main-content');
-            mainContentElement.innerHTML = newContent;
-}
